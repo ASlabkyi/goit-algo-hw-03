e414bf887204667952b06556c0e580ae17eb6847
Andrii Slabkyi <145043179+Polash0t@users.noreply.github.com>
2026-03-09 15:51:33 +0200

    Fixed 2 task

--- Task_2.py
+++ Task_2.py
@@ -1,7 +1,7 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or quantity < min or quantity > max or quantity > (max - min + 1):
+    if min < 1 or max > 1000 or min > max or quantity > (max - min + 1):
         return []
     else:
         result = random.sample(range(min, max+1), quantity)
